d34a4b825fd2a4022588883cd6d892d47ddef9fc
Jonathan Fodi <masterfodi@gmail.com>
2026-08-13 01:52:30 -0400

    top k and new_malicious_connections with connection_timestamps

--- cyber.py
+++ cyber.py
@@ -2,6 +2,7 @@ from bisect import bisect_left
 from collections import defaultdict
 from datetime import datetime, timedelta
 from typing import NamedTuple
+from heap import heappush, heappop
 
 
 class AuthLog(NamedTuple):
@@ -34,7 +35,8 @@ class Cyber():
         self.new_malicious_connections = set() # [ (src_ip, malicious_dest_ip) ]
         self.old_malicious_connections = set() # [ (src_ip, malicious_dest_ip) ]
         self.new_threats = set() # latest_threats - curr_threats
-        self.connection_timestamps = defaultdict(list) # dest_ip : [ (src_host, timestamp) ]
+        # self.connection_timestamps = defaultdict(list) # dest_ip : [ (src_host, timestamp) ]
+        self.connection_timestamps = defaultdict(lambda: defaultdict(list)) # dest_ip : { src_host : [ timestamp ] }
 
         self.ingest_auth_logs(auth_logs)
         self.ingest_conn_logs(connection_logs)
@@ -60,8 +62,7 @@ class Cyber():
         timestamp, src_host, dst_ip, dst_port, bytes_sent = conn_log
         if dst_ip in self.threat_intel:
             self.malicious_connections.add((src_host, dst_ip))
-        self.connection_timestamps[dst_ip].append((src_host, timestamp))
-
+        self.connection_timestamps[dst_ip][src_host].append(timestamp)
 
     def failed_attempts_in_window(self, N, now, window):
         cutoff = now - window # 12:00 PM
@@ -79,7 +80,7 @@ class Cyber():
         
         return sorted(res, key=lambda x: (-x[1], x[0]))
 
-    def malicious_ip_connections(self):
+    def get_malicious_ip_connections(self):
         # dont return the internal strucutre
         # return self.malicious_connections
 
@@ -108,12 +109,12 @@ class Cyber():
         self.malicious_connections.difference_update(self.old_malicious_connections)
 
             
-        # add any connections with a new malicious IP to malicious connections
-        # we have a refresh interval of 1 hour so we should bisect the connection logs on that cutoff
-        now = datetime(2024, 1, 1, 17, 0)  # sample data's connection logs top out at 16:59
-        cutoff = now - self.threat_intel_refresh_interval  # e.g. 17:00 - 1hr = 16:00
-        # bisect_left, not bisect_right: entries == cutoff must stay in the window
-        start = bisect_left(self.connection_logs, cutoff, key=lambda f: f[0])
+        # # add any connections with a new malicious IP to malicious connections
+        # # we have a refresh interval of 1 hour so we should bisect the connection logs on that cutoff
+        # now = datetime(2024, 1, 1, 17, 0)  # sample data's connection logs top out at 16:59
+        # cutoff = now - self.threat_intel_refresh_interval  # e.g. 17:00 - 1hr = 16:00
+        # # bisect_left, not bisect_right: entries == cutoff must stay in the window
+        # start = bisect_left(self.connection_logs, cutoff, key=lambda f: f[0])
         
         
         # for log in self.connection_logs[start:]:
@@ -128,13 +129,24 @@ class Cyber():
         #         self.new_malicious_connections.add((src_host, dst_ip))
         
         for ip in self.new_threats:
-            if ip in self.connection_timestamps.keys():
-                conn_list = self.connection_timestamps[ip]
-                for conn in conn_list:
-                    src_host, timestamp = conn
+            if ip in self.connection_timestamps:
+                ip_conn_dict = self.connection_timestamps[ip] # { src_host: [timestamps] }
+                # for every src in this list, we want to add (src_host, dst_ip) 
+                for src_host in ip_conn_dict:
                     self.new_malicious_connections.add((src_host, ip))
                     self.malicious_connections.add((src_host, ip))
 
-
     def get_latest_threat_report(self) -> tuple[set, set]:
         return (set(self.new_malicious_connections), set(self.old_malicious_connections))
+
+    # return the K most 
+    def top_k_connections(self, k) -> list:
+        heap = []
+
+        for dest_ip in self.connection_timestamps:
+            for src_host in self.connection_timestamps[dest_ip]:
+                heappush(heap, (len(self.connection_timestamps[dest_ip][src_host]), src_host, dest_ip))
+                if len(heap) > k:
+                    heappop(heap)
+        
+        return sorted(heap, reverse=True)
